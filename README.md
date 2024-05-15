6efe396ac7d435ff22fd2dcc89c61d43b7a1597c
Daniel Marques <dnmarques@gmail.com>
2024-05-15 14:32:54 +0000

    Project improvements

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # ssp_analysis
 Projeto usado para analisar os dados de veículos e celulares subtraídos em São Paulo, divulgados pela Secretaria de Segurança Pública de São Paulo (SSP SP).
+
+Este projeto nasceu de uma curisidade com relação a violência em São Paulo e para que eu possa transformar um dado em informação e com isso, tomar decisões e precauções com relação a comportamento em determinadas regiões.
+
+## Disclaimer
+
+Este conteúdo é fornecido apenas para fins informativos e educacionais. Não deve ser interpretado como aconselhamento profissional ou utilizado para fins comerciais. Todas as informações apresentadas são fornecidas de boa fé e são baseadas em fontes consideradas confiáveis. No entanto, não garantimos a precisão, integridade ou atualidade dessas informações. Os usuários são incentivados a realizar suas próprias pesquisas e consultar profissionais qualificados para obter orientação específica para suas necessidades. Não nos responsabilizamos por quaisquer perdas ou danos decorrentes do uso ou confiança nas informações fornecidas neste conteúdo.
+
+## Fontes de Dados
+
+As informações informações utilizadas nesse projeto foram obtidas através do portal da transparêncoa da Secretaria de Segurança Pública de São Paulo.
+
+Link [text](https://www.ssp.sp.gov.br/estatistica/consultas)
+
+Para reduzir o tamanho do arquivo e deixá-lo com menos de 50MB e em formato CSV, separado por ";", foi feita a importação do arquivo original no formato "xlsx" e deixadas as seguintes colunas:
+
+Arquivo: CelularesSubtraidos_2023.csv
+
+    #   Column                
+    --  -------------------                
+    0   NUM_BO                
+    1   DATA_OCORRENCIA_BO    
+    2   HORA_OCORRENCIA       
+    3   DATAHORA_REGISTRO_BO  
+    4   DESCR_TIPOLOCAL       
+    5   CIDADE                
+    6   BAIRRO                
+    7   LOGRADOURO            
+    8   LATITUDE              
+    9   LONGITUDE             
+    10  QUANTIDADE_OBJETO     
+    11  MARCA_OBJETO          
+
+Arquivo: VeiculosSubtraidos_2023.csv
+
+    #   Column                
+    --  --------------------               
+    0   NUM_BO               
+    1   DATA_OCORRENCIA_BO   
+    2   HORA_OCORRENCIA      
+    3   DATAHORA_REGISTRO_BO 
+    4   DESCR_TIPOLOCAL      
+    5   CIDADE               
+    6   BAIRRO               
+    7   LOGRADOURO           
+    8   LATITUDE             
+    9   LONGITUDE            
+    10  DESCR_TIPO_VEICULO   
+    11  DESCR_MARCA_VEICULO  
+
+Nota: Não foi feita nenhuma manipulação e/ou tratamento de dados. Existem bairros como números, duplicados devido a acentuação, enfim. Este projeto não tem o intuito de ser o estado da arte, somente servir de exemplo de usabilidade do que daria para ser feito com informações públicas.
+
+Sinta-se a vontade para baixar os arquivos de outros anos e substituir e fazer suas analises. Não se esqueça de manter a mesma estrutura!
+
+Aproveite!
+
+Um grande abraço,
+Daniel Marques
+dnmarques@gmail.com
--- app.py
+++ app.py
@@ -7,14 +7,19 @@ import pandas as pd
 import altair as alt
 import calendar
 
+import pathlib
+
+PATH = pathlib.Path(__file__).parent.absolute()
+
+
 meses = {1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho',7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
 
 @st.cache_resource
 def loadData(servico):
     if servico == "Celulares Subtraídos":
-        return pd.read_csv("./data/CelularesSubtraidos_2023.csv", sep="|", low_memory=False)
+        return pd.read_csv(str(PATH) + "/data/CelularesSubtraidos.csv", sep=";", low_memory=False)
     elif servico == "Veículos Subtraídos":
-        return pd.read_csv("./data/VeiculosSubtraidos_2023.csv", sep="|", low_memory=False)
+        return pd.read_csv(str(PATH) +"/data/VeiculosSubtraidos.csv", sep=";", low_memory=False)
     else:
         return None
     
@@ -33,13 +38,14 @@ def build_sidebar():
     cidade = None
     bairro = None
     filtered_data = None
+    
+    st.image(str(PATH) + "/images/Logo_SSP.png")
 
-    st.image("./images/Logo_SSP.png")
     servico = st.sidebar.selectbox("Selecione o serviço:", ["Veículos Subtraídos","Celulares Subtraídos"])
 
     if servico:
         data = loadData(servico)
-        
+
         cidade = st.sidebar.selectbox("Selecione a cidade:", data["CIDADE"].unique())
 
         if cidade:
@@ -52,6 +58,11 @@ def build_sidebar():
             if bairro and bairro != "Todos":
                 filtered_data = filtered_data[filtered_data["BAIRRO"] == bairro]
 
+    footer_html = """<div style='text-align: left;'>
+                        <p>Developed with Streamlit by Daniel Marques</p>
+                    </div>"""
+    st.markdown(footer_html, unsafe_allow_html=True)
+
     return servico, cidade, bairro, filtered_data
 
 
@@ -61,13 +72,16 @@ def build_main(servico, cidade, bairro, filtered_data):
 
         col1, col2 = st.columns(2, gap='large')
         with col1:
-            st.markdown("Mapa de calor das ocorrências")
+            st.subheader("Mapa de calor das ocorrências")
             m = folium.Map(location=[-23.55, -46.63], zoom_start=10)
             HeatMap(data=[[row['LATITUDE'], row['LONGITUDE']] for index, row in filtered_data.iterrows()], radius=10).add_to(m)
             folium_static(m)
 
         with col2:
-            st.markdown("Evolução das ocorrências ao longo do ano")
+            st.subheader("Evolução das ocorrências ao longo do ano")
+
+            filtered_data['DATA_OCORRENCIA_BO'] = pd.to_datetime(filtered_data['DATA_OCORRENCIA_BO'], format='%Y-%m-%d')
+            filtered_data['MES'] = filtered_data['DATA_OCORRENCIA_BO'].dt.month            
 
             monthly_counts = filtered_data.groupby("MES").size()
             monthly_counts = monthly_counts.reset_index().rename(columns={"MES": "Mês", 0: "Quantidade"})
@@ -85,23 +99,23 @@ def build_main(servico, cidade, bairro, filtered_data):
         with col21:
 
             if bairro == "Todos":
-                st.write("Bairros com maior número de ocorrências")
+                st.subheader("Bairros com maior número de ocorrências")
                 ocorrenciasPorBairro = filtered_data["BAIRRO"].value_counts().reset_index().rename(columns={"BAIRRO": "Bairro", "count": "Quantidade"})
                 st.dataframe(ocorrenciasPorBairro,hide_index=True,use_container_width=True)
             else:
-                st.write(f"Endereços com maiores ocorrências no bairro {bairro}")
+                st.subheader(f"Endereços com maiores ocorrências no bairro {bairro}")
                 ocorrenciasPorLogradouro = filtered_data["LOGRADOURO"].value_counts().reset_index().rename(columns={"LOGRADOURO": "Logradouro", "count": "Quantidade"})
                 st.dataframe(ocorrenciasPorLogradouro,hide_index=True,use_container_width=True)
 
         with col22:
             if servico == "Veículos Subtraídos":
-                st.markdown("Automóveis mais subtraídos")
+                st.subheader("Automóveis mais subtraídos")
                 veículosMaisRoubados = filtered_data["DESCR_MARCA_VEICULO"].value_counts().nlargest(15).reset_index()
                 veículosMaisRoubados.columns = ["Marca/Modelo", "Qtde"]
                 st.dataframe(veículosMaisRoubados,hide_index=True,use_container_width=True)
 
             elif servico == "Celulares Subtraídos":
-                st.markdown("Celulares mais subtraídos")
+                st.subheader("Celulares mais subtraídos")
                 celularesMaisRoubados = filtered_data["MARCA_OBJETO"].value_counts().nlargest(15).reset_index()
                 celularesMaisRoubados.columns = ["Aparelho/Modelo", "Qtde"]
                 st.dataframe(celularesMaisRoubados,hide_index=True,use_container_width=True)
@@ -113,7 +127,7 @@ with st.sidebar:
     servico, cidade, bairro, filtered_data = build_sidebar()
 
 st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
-st.header("Ocorrências 2023 - Secretaria de Segurança Pública de São Paulo")
+st.header("Ocorrências - Secretaria de Segurança Pública de São Paulo")
 
 if filtered_data is not None:
     build_main(servico, cidade, bairro, filtered_data)
